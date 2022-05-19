bbc64e8cfd1ab3ccf2db441028411c0860101be0
Tanguy Marlot <35462932+artshow@users.noreply.github.com>
2022-05-19 22:17:49 +0200

    move style button skills

--- src/components/searchBar/index.js
+++ src/components/searchBar/index.js
@@ -3,7 +3,7 @@ import './styles.css';
 const SearchBar = ({placeholder='', onChange}) => {
 	return (
 		<div className='search__bar'>
-			<input onChange={onChange} type="text" className='search__bar-input' placeholder={placeholder} />
+			<input onChange={onChange} type='text' className='search__bar-input' placeholder={placeholder} />
 			<img className='search__bar-icon' src='/icons/icon-search.svg' />
 		</div>
 	);
